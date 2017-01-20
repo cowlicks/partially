ef9aa845ce8e00db2f55b6c1929cab14d22425b9
Blake Griffith <cowlicks@riseup.net>
2017-01-19 23:30:14 -0800

    Finish the protocol, but it is broken.

--- partially.py
+++ partially.py
@@ -98,12 +98,27 @@ def choose_keypair(parameters):
     x = rand_less_than(parameters.q, parameters.N)
     return Keypair(x, pow(parameters.g, x, parameters.p), parameters)
 
-def digest(info, parameters):
+def do_hash(data):
     h = hashlib.sha256()
-    h.update(info)
-    hbytes = h.digest()
-    hint = int.from_bytes(hbytes, byteorder='little')
-    return pow(hint, (p - 1)//q, p)
+    h.update(data)
+    return h.digest()
+
+def digest(data, parameters):
+    i = int.from_bytes(do_hash(data), byteorder='little')
+    return pow(i, (parameters.p - 1)//parameters.q, parameters.p)
+
+def int_to_bytes(in_int):
+    i = in_int
+    byte_length = ((i).bit_length() + 7) // 8
+    return i.to_bytes(byte_length, 'little')
+
+def full_domain_hash(data, target_length):
+    digest_size = 32
+    ncycles = (target_length // digest_size) + 1
+    out = bytearray()
+    for i in range(ncycles):
+        out.extend(do_hash(data + int_to_bytes(i)))
+    return bytes(out[:target_length])
 
 class Signer:
     def __init__(self, parameters):
@@ -111,8 +126,9 @@ class Signer:
         self.L, self.N, self.p, self.q, self.g = tuple(parameters)
         self.keypair = choose_keypair(self.parameters)
 
-    def step_one(self, info):
-        self.u, self.s, self.d = [rand_less_than(self.q, self.N) for _ in range(3)]
+    def one(self, info):
+        usd = [rand_less_than(self.q, self.N) for _ in range(3)]
+        self.u, self.s, self.d = usd
 
         self.z = digest(info, self.parameters)
 
@@ -120,9 +136,70 @@ class Signer:
         self.b = pow(self.g, self.s, self.p) * pow(self.z, self.d, self.p) % self.p
         return self.a, self.b
 
+    def three(self, e):
+        self.c = (e - self.d) % self.q
+        self.r = (self.u - (self.c * self.keypair.x)) % self.q
+        return self.r, self.c, self.s, self.d
+
+class User:
+    def __init__(self, parameters, pubkey):
+        self.parameters = parameters
+        self.L, self.N, self.p, self.q, self.g = tuple(parameters)
+        self.y = pubkey
+
+    def start(self, info, msg):
+        ts = [rand_less_than(self.q, self.N) for _ in range(4)]
+        self.t1, self.t2, self.t3, self.t4 = ts
+
+        self.z = digest(info, self.parameters)
+        self.msg = msg
+
+    def two(self, a, b):
+        alpha = a * pow(self.g, self.t1, self.p) * pow(self.y, self.t2, self.p) % self.p
+        beta = b * pow(self.g, self.t3, self.p) * pow(self.z, self.t4, self.p) % self.p
+
+        e_bytes = bytearray()
+        for v in (alpha, beta, self.z):
+            e_bytes.extend(int_to_bytes(v))
+        e_bytes.extend(msg)
+
+        epsilon = int.from_bytes(full_domain_hash(e_bytes, self.N), 'little')
+        return (epsilon - self.t2 - self.t4) % self.q
+
+    def four(self, r, c, s, d):
+        rho = (r + self.t1) % self.q
+        omega = (c + self.t2) % self.q
+        delta = (s + self.t3) % self.q
+        sigma = (d + self.t4) % self.q
+        return rho, omega, delta, sigma
+
+def check(rho, omega, delta, sigma, z, msg, y, parameters):
+    one = (pow(parameters.g, rho, parameters.p) *
+           pow(y, omega, parameters.p)) % parameters.p
+    two = (pow(parameters.g, delta, parameters.p) *
+           pow(z, sigma, parameters.p)) % parameters.p
+
+    rhs = full_domain_hash(
+            int_to_bytes(one) + int_to_bytes(two) + int_to_bytes(z) + msg,
+            parameters.N)
+    lhs = int_to_bytes((omega + sigma) % parameters.p)
+
+    return rhs == lhs
+
 if __name__ == '__main__':
     L, N = 1024, 160
-    params = choose_parameters(L, N)
-    kp = choose_keypair(params)
+    info = b'info'
+    msg = b'my msg'
 
-    print(tuple(params))
+    params = choose_parameters(L, N)
+    signer = Signer(params)
+
+    user = User(params, signer.keypair.y)
+    user.start(info, msg)
+
+    a, b = signer.one(b'info')
+    e = user.two(a, b)
+    r, c, s, d = signer.three(e)
+    rho, omega, delta, sigma = user.four(r, c, s, d)
+
+    print(check(rho, omega, delta, sigma, user.z, msg, user.y, params))
